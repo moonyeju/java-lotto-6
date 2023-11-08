666a47213082def5da17d50a1c07357501dba636
moonyeju <moonyeju8510@gmail.com>
2023-11-08 15:56:26 +0900

    [FEAT] 로또 구입 금액 입력 기능 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,35 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        int purchaseAmount = getPurchaseAmount();
+        System.out.println(purchaseAmount + "원");
+
     }
+    private static int getPurchaseAmount() {
+        while (true) {
+            try {
+                System.out.println("구입금액을 입력해 주세요.");
+                int purchaseAmount = Integer.parseInt(Console.readLine());
+                validatePurchaseAmount(purchaseAmount);
+                return purchaseAmount;
+            } catch (NumberFormatException e) {
+                System.out.println("[ERROR] 숫자를 입력해 주세요.");
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+
+        }
+    }
+    private static void validatePurchaseAmount(int purchaseAmount) {
+        if (purchaseAmount % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 1000원 단위로 입력해 주세요.");
+        }
+    }
+
+
 }
